41ffb43b1f5a304d5b8ed537cc0bd7ca9244118d
IsmailDhanto <dhanto05@gmail.com>
2023-03-06 09:38:52 +0300

    commited

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 var selectedRaw = null;
-function onFormSubmit(){
+function onFormSubmit(e){
+    event.preventDefault();
+    var FormData = readFromData();
+    if (selectedRaw === null){
+        insertData(FormData)
+    }
+    else{
+        
+    }
 
 }
 
@@ -22,6 +30,16 @@ function insertData(data){
     var table = document.createElement("storelist").getElementsByTagName('tbody')[0];
     var newRaw = table.insertRaw(table.lenght);
     var cell1 = newRaw.insertCell(0);
+        cell1.innerHTML = data.pruductCode;
+    var cell2 = newRaw.insertCell(1);
+        cell2.innerHTML = data.product;
+    var cell3 = newRaw.insertCell(2);
+        cell3.innerHTML = data.qty;
+    var cell4 = newRaw.insertCell(3);
+        cell4.innerHTML = data.perprice;
+    var cell5 = newRaw.insertCell(4);
+        cell5.innerHTML = '<button>Edit</button> <button>Delete</button>' 
+   
     
 
 }
